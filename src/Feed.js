a55329bfaf6dd67b131588a979d3562c230531b6
iravzdhanju <52969271+iravzdhanju@users.noreply.github.com>
2021-02-05 11:01:20 -0500

    Update feed.js

--- src/Feed.js
+++ src/Feed.js
@@ -14,14 +14,16 @@ function Feed() {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
-    db.collection('posts').onSnapshot((snapshot) =>
-      setPosts(
-        snapshot.docs.map((doc) => ({
-          id: doc.id,
-          data: doc.data(),
-        }))
-      )
-    );
+    db.collection('posts')
+      .orderBy('timestamp', 'desc')
+      .onSnapshot((snapshot) =>
+        setPosts(
+          snapshot.docs.map((doc) => ({
+            id: doc.id,
+            data: doc.data(),
+          }))
+        )
+      );
   }, []);
 
   const sendPost = (e) => {
